62d2d9c68daa45c99ca9ae7436357645cf99b049
david34Y <dgonzalesn@pucp.edu.pe>
2022-11-08 21:09:17 -0500

    Controllers

---
+++ lab8Gtics/src/main/java/com/example/lab8gtics/Controller/PeliculasController.java
@@ -0,0 +1,18 @@
+package com.example.lab8gtics.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping(value = {"/peliculas"})
+public class PeliculasController {
+
+
+
+}
---
+++ lab8Gtics/src/main/java/com/example/lab8gtics/Controller/ReservasController.java
@@ -0,0 +1,8 @@
+package com.example.lab8gtics.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservasController {
+
+}
